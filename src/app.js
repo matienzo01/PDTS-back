13f19c3eed0845fce0610124060c71cf820edcf3
matias <matiasgutierrez225@gmail.com>
2024-02-05 00:36:23 -0300

    Arreglado problema del cors

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const database = require('./database/database.js')
 const routerDimensiones = require('./routes/rutas_dimensiones.js')
 const routerEvaluacion = require('./routes/rutas_evaluacion.js')
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(cors())
 app.use('/api/dimensiones', routerDimensiones)
 app.use('/api/evaluacion', routerEvaluacion)
 
