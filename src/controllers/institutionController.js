c084cd71f2b9f7b2faaf201e4fd67d11d3b368ca
AgustinPatane <143288585+AgustinFPatane@users.noreply.github.com>
2024-02-08 21:20:41 -0300

    post Instituciones
    Funciona pasando en el body un JSON como este:
    
    {
    	"admin" : {
    		"nombre": "Adrian",
    		"apellido": "Lopez",
    		"email": "AdrianLopez@gmail.com",
    		"password": ""
    	},
    	"institucion": {
    		"id_tipo": 1,
    		"nombre": "Facultad de Ingenieria",
    		"pais": "Argentina",
    		"provincia": "Buenos Aires",
    		"localidad": "Mar del plata",
    		"telefono_institucional": "2236887998",
    		"mail_institucional": "FIngenieria@fi.mdp.edu.ar"
    	}
    }

--- src/controllers/institutionController.js
+++ src/controllers/institutionController.js
@@ -1,17 +1,37 @@
 const servicio = require('../services/institutionService.js')
 
 const getOneInstitucion = async(req,res) => {
+    const { params: { id_institucion }} = req
+
+    if (!id_institucion) return;
     
+    try {
+        res.status(200).json(await servicio.getOneInstitucion(id_institucion))
+      } catch (error) {
+        console.error(`Error al obtener la institucion con id ${id_institucion}`, error)
+        res.status(500).json({error: 'Error'})
+      }
     return;
 }
 
 const getAllInstituciones = async(req,res) => {
     
-    return;
+    try {
+        const instituciones = await servicio.getAllInstituciones()
+        res.status(200).json({instituciones})
+      } catch (error) {
+        console.error('Error al obtener las instituciones', error)
+        res.status(500).json({error: 'Error'})
+      }
 }
 
 const createInstitucion = async(req,res) => {
-    
+    const { admin, institucion } = req.body
+    try {
+        res.status(200).json(await servicio.createInstitucion(admin,institucion))
+    } catch(error) {
+
+    }
     return;
 }
 
---
+++ src/services/institutionService.js
@@ -0,0 +1,70 @@
+const gen_consulta = require('../database/gen_consulta')
+const TABLA = 'instituciones'
+
+const getOneInstitucion = async(id_institucion) => {
+    const condiciones = [`id = ${id_institucion}`]
+    try {
+        return await gen_consulta._select(TABLA,null,condiciones)
+    } catch (error) {
+        throw error;
+    }
+}
+
+const getAllInstituciones = async() => {
+    try {
+        return await gen_consulta._select(TABLA,null,null)
+    } catch (error) {
+        throw error;
+    }
+}
+
+const createInstitucion = async(newAdmin,institucion) => {
+    
+    const institutions_table = `instituciones`
+    const admins_table = 'admins_cyt(nombre,apellido,email,password)'
+    const conds = [
+        `nombre = '${institucion.nombre}'`,
+        `pais = '${institucion.pais}'`,
+        `provincia = '${institucion.provincia}'`,
+        `localidad = '${institucion.localidad}'`,
+    ]
+
+    const exists = await gen_consulta._select(institutions_table,null,conds)
+    if (exists[0] === undefined) //no existe todavia la institucion
+    {   
+        try {
+            const admin = await gen_consulta._insert(admins_table,Object.values(newAdmin))
+
+            const newInstitution = {
+                id_tipo: institucion.id_tipo, //esto habria que cambiarlo creo
+                id_admin: admin.insertId,
+                nombre: institucion.nombre,
+                pais: institucion.pais,
+                provincia: institucion.provincia,
+                localidad: institucion.localidad,
+                telefono_institucional: institucion.telefono_institucional,
+                mail_institucional: institucion.mail_institucional
+            }
+
+            return await gen_consulta._insert(institutions_table
+                .concat('(id_tipo,id_Admin,nombre,pais,provincia,localidad,telefono_institucional,mail_institucional)'),
+                Object.values(newInstitution))
+
+        } catch(error) {
+            throw error;
+        }
+    }
+    return 'ya existe esa institucion';
+}
+
+const deleteInstitucion = async() => {
+    
+    return;
+}
+
+module.exports = {
+    getOneInstitucion,
+    getAllInstituciones,
+    createInstitucion,
+    deleteInstitucion
+}
