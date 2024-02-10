ffca180d14cbfcc7375d10ffd5cf3773a6f26d94
AgustinPatane <143288585+AgustinFPatane@users.noreply.github.com>
2024-02-10 16:46:53 -0300

    alta de evaluadores

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -14,12 +14,32 @@ const getAllInstitutionUsers = async(req,res) => {
     try {
         res.status(200).json(await service.getAllInstitutionUsers(id_institucion))
     } catch(error) {
-
+        res.status(500).json({error: `Error getting the users from the institution`})
     }
 }
 
 const createUser = async(req,res) => {
     const {params: { id_institucion }} = req
+    const { type } = req.body
+    if (type === 'dni') {
+
+        const { dni } = req.body
+        try {
+            res.status(200).json(await service.linkUserToInstitution(dni,id_institucion))
+        } catch(error) {
+            res.status(500).json({error: error.message})
+        }
+
+    } else if (type === 'user') {
+        const { user } = req.body
+        try {
+            res.status(200).json(await service.createUser(user,id_institucion))
+        } catch(error){
+            res.status(500).json({error: error.message})
+        }
+    } else {
+        res.status(400).json({error: 'Bad request'})
+    }
 
 
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -23,11 +23,51 @@ const getAllInstitutionUsers = async (id_institucion) => {
 
 }
 
-const createUser = async () => {
 
+
+const createUser = async (newUser,institutionId) => {
+    
+    try {
+        const user = await getUserByDni(newUser.dni)
+        if (user !== undefined) {// existe un usuario con ese dni
+            throw new Error('There is already a user with that DNI')
+        }
+        const table1 = 'evaluadores(email,nombre,apellido,dni,celular,especialidad,institucion_origen,pais_residencia,provincia_residencia,localidad_residencia)'
+        const insertData = await gen_consulta._insert(table1,Object.values(newUser))
+        return await linkUserToInstitution(newUser.dni,institutionId)
+
+    } catch(error){
+        throw new Error(error.message)
+    }
+}
+
+const getUserByDni = async (dni) => {
+    try {
+        const user = await gen_consulta._select('evaluadores',null,[`dni = ${dni}`])
+        return user[0]
+    } catch(error) {
+        throw error
+    }
+}
+
+const linkUserToInstitution = async(userDni, institutionId) => {
+    
+    const table = 'evaluadores_x_instituciones(id_institucion,id_evaluador)'
+    const user = await getUserByDni(userDni)
+    
+    if (user === undefined) {
+        throw new Error('There is no user with the provided DNI');
+    }
+
+    try {
+        return await gen_consulta._insert(table,[institutionId,user.id])
+    } catch(error) {
+        throw error
+    }
 }
 
 module.exports = {
     getAllInstitutionUsers,
-    createUser
+    createUser,
+    linkUserToInstitution
 }
