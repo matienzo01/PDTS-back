41de48a3e717f300e3f5db381820a343af22d251
matienzo01 <matiasgutierrez225@gmail.com>
2024-02-25 13:32:29 -0300

    el login retorna el userForToken como data del usuario

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -2,47 +2,48 @@ const bcrypt = require('bcrypt')
 const knex = require('../database/knex')
 const jwt = require('jsonwebtoken')
 
-const login = async (mail,password) => {
-    
-    const tablesToCheck = [
-        { tableName: 'admin', columns: ['email', 'password'] },
-        { tableName: 'admins_cyt', columns: ['id', 'email', 'password'] },
-        { tableName: 'evaluadores', columns: ['id', 'email', 'password'] }
-    ];
-    
-    let user = null;
-    
-    for (const table of tablesToCheck) {
-        user = await knex(table.tableName).select(...table.columns).where({ email: mail }).first();
-        if (user) {
-            console.log(table.tableName)
-            if (table.tableName === 'admin') {
-                user.id = 1;
-            }
-            break;
-        }
-    }
+const login = async (mail, password) => {
 
-    const passwordCorrect = user === undefined
-        ? false
-        : await bcrypt.compare(password, user.password)
-    
-    delete user.password
-    if (!(user && passwordCorrect)) {
-        const _error = new Error('Invalid user or password')
-        _error.status = 401
-        throw _error
-    }
+  const tablesToCheck = [
+    { tableName: 'admin', columns: ['email', 'password'] },
+    { tableName: 'admins_cyt', columns: ['id', 'email', 'password'] },
+    { tableName: 'evaluadores', columns: ['id', 'email', 'password'] }
+  ];
 
-    const userForToken = {
-        id: user.id,
-        mail: user.email
-    }
+  let user = null;
 
-    const token = jwt.sign(userForToken, process.env.SECRET || 'clave')
-    return { token: token}
+  for (const table of tablesToCheck) {
+    user = await knex(table.tableName).select(...table.columns).where({ email: mail }).first();
+    if (user) {
+      console.log(table.tableName)
+      if (table.tableName === 'admin') {
+        user.id = 1;
+      }
+      break;
+    }
+  }
+
+  const passwordCorrect = user === undefined
+    ? false
+    : await bcrypt.compare(password, user.password)
+
+  delete user.password
+  if (!(user && passwordCorrect)) {
+    const _error = new Error('Invalid user or password')
+    _error.status = 401
+    throw _error
+  }
+
+  const userForToken = {
+    id: user.id,
+    mail: user.email,
+    institutionId: 1
+  }
+
+  const token = jwt.sign(userForToken, process.env.SECRET || 'clave')
+  return { token: token, user: userForToken }
 }
 
 module.exports = {
-    login
+  login
 }
