1dc72e06209eb4c5a312201099f75efae25f6fbb
AgustinPatane <143288585+AgustinFPatane@users.noreply.github.com>
2024-02-26 23:00:38 -0300

    pequeño cambio al login para que se devuelva el rol correspondiente

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -5,9 +5,9 @@ const jwt = require('jsonwebtoken')
 const login = async (mail, password) => {
 
   const tablesToCheck = [
-    { tableName: 'admin', columns: ['email', 'password'] },
-    { tableName: 'admins_cyt', columns: ['id', 'email', 'password'] },
-    { tableName: 'evaluadores', columns: ['id', 'email', 'password'] }
+    { rol: 'admin general', tableName: 'admin', columns: ['email', 'password'] },
+    { rol: 'admin', tableName: 'admins_cyt', columns: ['id', 'email', 'password'] },
+    { rol: 'evaluador', tableName: 'evaluadores', columns: ['id', 'email', 'password'] }
   ];
 
   let user = null;
@@ -15,10 +15,12 @@ const login = async (mail, password) => {
   for (const table of tablesToCheck) {
     user = await knex(table.tableName).select(...table.columns).where({ email: mail }).first();
     if (user) {
-      console.log(table.tableName)
+      user.rol = table.rol
       if (table.tableName === 'admin') {
         user.id = 1;
-      }
+      } else if (table.tableName === 'admins_cyt') {
+        user.institutionId = (await knex('instituciones_cyt').select('id').where({id_admin: user.id}).first()).id
+      } 
       break;
     }
   }
@@ -37,8 +39,8 @@ const login = async (mail, password) => {
   const userForToken = {
     id: user.id,
     mail: user.email,
-    rol: 'admin',
-    institutionId: 1
+    rol: user.rol,
+    institutionId: user.institutionId
   }
 
   const token = jwt.sign(userForToken, process.env.SECRET || 'clave')
