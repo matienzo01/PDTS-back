d4570c9f3698fd16ecdd9c156a649f7beaa3bfed
AgustinPatane <143288585+AgustinFPatane@users.noreply.github.com>
2024-04-24 18:02:54 -0300

    Update customError.ts

--- src/types/customError.ts
+++ src/types/customError.ts
@@ -2,6 +2,7 @@ export interface CustomErrorInterface extends Error {
     status: number;
 }
 
+
 export class CustomError extends Error implements CustomErrorInterface {
     public status: number;
     constructor(message: string, code: number) {
