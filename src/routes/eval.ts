626747451e9987c686fcfc8b77f9491048e47bad
AgustinPatane <143288585+AgustinFPatane@users.noreply.github.com>
2024-04-24 12:51:46 -0300

    Ahora se guardan ambas instancias del formulario a la vez
    post en /api/evaluacion/form/:id_proyecto

--- src/routes/eval.ts
+++ src/routes/eval.ts
@@ -5,11 +5,9 @@ import authUser from '../middlewares/authUser'
 import checkRol from '../middlewares/checkRol'
 
 router
-    // nuevos
     .get('/entidad/:id_proyecto', authUser, checkRol(['evaluador', 'admin']), controller.getEntidad)
-    .post('/entidad/:id_proyecto', authUser, checkRol(['evaluador']), controller.postEntidad)
     .get('/proposito/:id_proyecto', authUser, checkRol(['evaluador', 'admin']), controller.getProposito)
-    .post('/proposito/:id_proyecto', authUser, checkRol(['evaluador']), controller.postProposito)
+    .post('/form/:id_proyecto', authUser, checkRol(['evaluador']), controller.saveForm)
 
     .get('/:id_proyecto/respuestas/pdf', authUser, checkRol(['admin']), controller.generatePDF)
     .put('/:id_proyecto/finalizar', authUser, checkRol(['evaluador','admin']), controller.finalizarEvaluacion)
