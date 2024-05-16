47c2f368c73c63a6ccd23b606d289715f4ac8f55
AgustinPatane <143288585+AgustinFPatane@users.noreply.github.com>
2024-05-16 12:43:50 -0300

    Update knex.ts

--- src/database/knex.ts
+++ src/database/knex.ts
@@ -1,13 +1,27 @@
 import Knex from 'knex';
 
+/*
 const knex = Knex({
   client: 'mysql',
   connection: {
-    port: 2086,
+    port: 3306,
     host: 'localhost',
     user: 'seva',
     password: '20ProySe-va24',
     database: 'seva-pdts'
+  }
+});*/
+
+
+
+const knex = Knex({
+  client: 'mysql',
+  connection: {
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pdts'
   }
 });
 
