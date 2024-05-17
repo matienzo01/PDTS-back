b575ded68850790fb4f839c535d500e9ea86319a
AgustinPatane <143288585+AgustinFPatane@users.noreply.github.com>
2024-05-17 13:46:20 -0300

    cambio a https

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import https from 'https'
+import fs from 'fs';
 import dotenv from 'dotenv'
 dotenv.config();
 import express from 'express'
@@ -9,8 +11,12 @@ import routerInst from './routes/institution';
 import routerUser from './routes/user';
 import routerEncuesta from './routes/encuesta';
 import cors from 'cors';
+
 const app = express();
 const PORT = 3000;
+const privateKey = fs.readFileSync('certificados/seva-pdts.ar.key');
+const certificate = fs.readFileSync('certificados/seva-pdts.ar.crt');
+const credentials = { key: privateKey, cert: certificate };
 
 app.use(express.json())
 app.use(cors())
@@ -23,8 +29,14 @@ app.use('/api/instituciones', routerInst)
 app.use('/api/usuarios', routerUser)
 app.use('/api/encuesta', routerEncuesta)
 
+const server = https.createServer(credentials, app);
+server.listen(PORT, () => {
+  console.log('funciona el server')
+});
+
+/*
 const server = app.listen(PORT, () => {
   console.log('funciona el server')
-})
+})*/
 
 module.exports = server
