236fcbc1de5a0a2bc0eb30fce2719838b0271486
AgustinPatane <143288585+AgustinFPatane@users.noreply.github.com>
2024-05-17 12:31:55 -0300

    Update knex.ts

--- src/database/knex.ts
+++ src/database/knex.ts
@@ -1,6 +1,6 @@
 import Knex from 'knex';
 
-
+// para conectarse a la base de datos de manera remota
 const knex = Knex({
   client: 'mysql',
   connection: {
@@ -12,7 +12,20 @@ const knex = Knex({
   }
 });
 
+// este debería ser el que se use definitivamente
 /*
+const knex = Knex({
+  client: 'mysql',
+  connection: {
+    port: 3306,
+    host: 'localhost',
+    user: 'seva',
+    password: '20ProySe-va24',
+    database: 'seva-pdts'
+  }
+});*/
+
+/* para conectarse con la base de datos localmente
 const knex = Knex({
   client: 'mysql',
   connection: {
