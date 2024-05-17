54f4ea97535eb8d234e2510b4207fcaaafd0163a
AgustinPatane <143288585+AgustinFPatane@users.noreply.github.com>
2024-05-17 13:48:03 -0300

    Update knex.ts
    dejo la configuracion de la conexión a la base de datos puesta como debería estar en producción, para usarla de otra forma hay que descomentar alguna de las otras dos opciones

--- src/database/knex.ts
+++ src/database/knex.ts
@@ -1,6 +1,7 @@
 import Knex from 'knex';
 
 // para conectarse a la base de datos de manera remota
+/*
 const knex = Knex({
   client: 'mysql',
   connection: {
@@ -10,10 +11,9 @@ const knex = Knex({
     password: '20ProySe-va24',
     database: 'seva-pdts'
   }
-});
+});*/
 
 // este debería ser el que se use definitivamente
-/*
 const knex = Knex({
   client: 'mysql',
   connection: {
@@ -23,7 +23,7 @@ const knex = Knex({
     password: '20ProySe-va24',
     database: 'seva-pdts'
   }
-});*/
+});
 
 /* para conectarse con la base de datos localmente
 const knex = Knex({
