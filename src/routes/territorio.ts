6a95c8f5d4b476d6274256eec1d1e28185f7342b
matienzo01 <matiasgutierrez225@gmail.com>
2024-06-23 15:00:32 -0300

    Los datos de los territorios son accesibles tambien por los evaluadores

--- src/routes/territorio.ts
+++ src/routes/territorio.ts
@@ -5,8 +5,8 @@ import checkRol from '../middlewares/checkRol'
 import controller from '../controllers/territorio'
 
 router
-  .get('/paises', authUser, checkRol(['admin', 'admin general']), controller.getPaises)
-  .get('/provincias', authUser, checkRol(['admin','admin general']), controller.getProvincias)
+  .get('/paises', authUser, checkRol(['evaluador', 'admin', 'admin general']), controller.getPaises)
+  .get('/provincias', authUser, checkRol(['evaluador','admin','admin general']), controller.getProvincias)
   .get('/localidades', authUser, checkRol(['admin','admin general']), controller.getAllLocalidades)
   .get('/:id_provincia/localidades', authUser, checkRol(['admin general']), controller.getLocalidades)
 
