def8988aba3d477b1823c668febfed50873e89f5
AgustinPatane <143288585+AgustinFPatane@users.noreply.github.com>
2024-06-28 19:27:12 -0300

    Update project.ts

--- src/services/project.ts
+++ src/services/project.ts
@@ -67,7 +67,7 @@ const getInstParticipants = async (id_proyecto: number, trx: any = null) => {
   const queryBuilder = trx || knex;
   const participaciones = await queryBuilder('participacion_instituciones')
     .join('instituciones', 'participacion_instituciones.id_institucion', 'instituciones.id')
-    .select('nombre as institucion', 'rol','pais','provincia','localidad','telefono_institucional','mail_institucional')
+    .select('nombre as institucion', 'rol','pais','provincia','localidad','telefono_institucional','mail_institucional', 'id')
     .where('participacion_instituciones.id_proyecto', id_proyecto)
   return participaciones
 }
